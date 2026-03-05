2e7efec303f30f48e1df368f588f32e9221aee4c
jieul2 <jieul2@kakao.com>
2026-03-06 00:28:32 +0900

    merge: 충돌해결 #9

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const meApi = require("./me.api");
 const newsApi = require("./news.api");
-
-router.use("/me", meApi);
-
 const authApi = require("./auth.api");
 const userApi = require("./user.api");
 
+
+router.use("/me", meApi);
 router.use("/auth", authApi);
 router.use("/me", userApi);
 router.use("/news", newsApi);
